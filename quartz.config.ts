5eea06d622c23f565543e0b6dfe31600a8ed9daa
SAJIB3489 <md.pramanic@edu.savonia.fi>
2024-05-26 01:17:37 +0300

    Quartz sync: May 26, 2024, 1:17 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧑🏻‍💻 Md. Sajib Pramanic",
+    pageTitle: "🧑🏻‍💻 Md. Sajib      Pramanic",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
